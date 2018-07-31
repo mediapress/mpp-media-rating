4566ddc80a3c4a5664d62bf374a2bee6c88e6dc2
Brajesh Singh <brajesh@cosmiccoders.com>
2018-07-31 09:43:58 +0530

    Update version

--- mpp-media-rating.php
+++ mpp-media-rating.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: MediaPress Media Rating
  * Plugin URI: https://buddydev.com/plugins/mpp-media-rating/
- * Version: 1.0.8
+ * Version: 1.0.9
  * Description: Used with MediaPress for rating on media.
  * Author: BuddyDev
  * Author URI: https://buddydev.com/
